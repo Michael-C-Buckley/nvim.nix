ce19e0f1ba796b34a103250b888bd545250f0fa5
Michael Buckley <michaelcbuckley@proton.me>
2026-04-20 05:34:01 -0400

    Nvim/Vscode: tweaked the binds and added some more useful things

--- nvim/lua/base/vscode_keymaps.lua
+++ nvim/lua/base/vscode_keymaps.lua
@@ -1,5 +1,6 @@
 -- Inspiration from Nick Maslov
 -- https://medium.com/@nikmas_dev/vscode-neovim-setup-keyboard-centric-powerful-reliable-clean-and-aesthetic-development-582d34297985
+-- With lots of additions and some improvements over the base
 
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
@@ -9,48 +10,119 @@ local vsc_action = function(action)
 	return "<cmd>lua require('vscode').action('" .. action .. "')<CR>"
 end
 
--- remap leader key
+-- leader
 keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- yank to system clipboard
-keymap(nv, "<leader>y", '"+y', opts)
+-- ====================
+-- Core Editing Quality
+-- ====================
 
--- paste from system clipboard
+-- system clipboard
+keymap(nv, "<leader>y", '"+y', opts)
 keymap(nv, "<leader>p", '"+p', opts)
 
--- better indent handling
+-- preserve yank on paste
+keymap("v", "p", '"_dP', opts)
+
+-- indentation
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
--- move text up and down
+-- move lines
 keymap("v", "J", ":m .+1<CR>==", opts)
 keymap("v", "K", ":m .-2<CR>==", opts)
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 
--- paste preserves primal yanked piece
-keymap("v", "p", '"_dP', opts)
-
--- removes highlighting after escaping vim search
+-- search UX
 keymap("n", "<Esc>", "<Esc>:noh<CR>", opts)
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
 
--- call vscode commands from neovim
+-- scroll center
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
 
--- general keymaps
-keymap(nv, "<leader>t", vsc_action("workbench.action.terminal.toggleTerminal"))
-keymap(nv, "<leader>b", vsc_action("editor.debug.action.toggleBreakpoint"))
-keymap(nv, "<leader>d", vsc_action("editor.action.showHover"))
-keymap(nv, "<leader>a", vsc_action("editor.action.quickFix"))
-keymap(nv, "<leader>sp", vsc_action("workbench.actions.view.problems"))
-keymap(nv, "<leader>cn", vsc_action("notifications.clearAll"))
-keymap(nv, "<leader>cp", vsc_action("workbench.action.showCommands"))
-keymap(nv, "<leader>pr", vsc_action("code-runner.run"))
-keymap(nv, "<leader>fd", vsc_action("editor.action.formatDocument"))
-keymap(nv, "<leader>ff", vsc_action("workbench.action.quickOpen"))
+-- ====================
+-- Navigation (LSP-like)
+-- ====================
 
--- harpoon keymaps
+keymap("n", "gd", vsc_action("editor.action.revealDefinition"), opts)
+keymap("n", "gD", vsc_action("editor.action.peekDefinition"), opts)
+keymap("n", "gr", vsc_action("editor.action.goToReferences"), opts)
+keymap("n", "gi", vsc_action("editor.action.goToImplementation"), opts)
+keymap("n", "gy", vsc_action("editor.action.goToTypeDefinition"), opts)
+
+-- diagnostics
+keymap("n", "]d", vsc_action("editor.action.marker.next"), opts)
+keymap("n", "[d", vsc_action("editor.action.marker.prev"), opts)
+
+-- ====================
+-- Leader Groups
+-- ====================
+
+-- ---- Files / Find ----
+keymap(nv, "<leader>ff", vsc_action("workbench.action.quickOpen"), opts)
+keymap(nv, "<leader>fg", vsc_action("workbench.action.findInFiles"), opts)
+keymap(nv, "<leader>fs", vsc_action("workbench.action.gotoSymbol"), opts)
+keymap(nv, "<leader>fS", vsc_action("workbench.action.showAllSymbols"), opts)
+
+-- ---- Buffers / Files ----
+keymap("n", "<leader>w", vsc_action("workbench.action.files.save"), opts)
+keymap("n", "<leader>q", vsc_action("workbench.action.closeActiveEditor"), opts)
+keymap("n", "<leader>bd", vsc_action("workbench.action.closeActiveEditor"), opts)
+
+-- ---- LSP / Code ----
+keymap(nv, "<leader>la", vsc_action("editor.action.quickFix"), opts)
+keymap(nv, "<leader>lc", vsc_action("editor.action.codeAction"), opts)
+keymap(nv, "<leader>lr", vsc_action("editor.action.rename"), opts)
+keymap(nv, "<leader>ld", vsc_action("editor.action.showHover"), opts)
+keymap(nv, "<leader>lf", vsc_action("editor.action.formatDocument"), opts)
+
+-- diagnostics panel
+keymap(nv, "<leader>lp", vsc_action("workbench.actions.view.problems"), opts)
+
+-- ---- Windows ----
+keymap("n", "<C-h>", vsc_action("workbench.action.focusLeftGroup"), opts)
+keymap("n", "<C-l>", vsc_action("workbench.action.focusRightGroup"), opts)
+keymap("n", "<C-k>", vsc_action("workbench.action.focusAboveGroup"), opts)
+keymap("n", "<C-j>", vsc_action("workbench.action.focusBelowGroup"), opts)
+
+keymap("n", "<leader>wh", vsc_action("workbench.action.moveEditorToLeftGroup"), opts)
+keymap("n", "<leader>wl", vsc_action("workbench.action.moveEditorToRightGroup"), opts)
+
+-- ---- Terminal ----
+keymap(nv, "<leader>tt", vsc_action("workbench.action.terminal.toggleTerminal"), opts)
+
+-- ---- Search / Selection ----
+keymap("n", "<leader>ss", vsc_action("workbench.action.findInFiles"), opts)
+keymap("n", "<leader>sw", vsc_action("editor.action.addSelectionToNextFindMatch"), opts)
+
+-- multicursor
+keymap("n", "<C-d>", vsc_action("editor.action.addSelectionToNextFindMatch"), opts)
+keymap("n", "<leader>mc", vsc_action("editor.action.insertCursorBelow"), opts)
+
+-- ---- Comments ----
+keymap("n", "<leader>/", vsc_action("editor.action.commentLine"), opts)
+keymap("v", "<leader>/", vsc_action("editor.action.commentLine"), opts)
+
+-- ---- Git ----
+keymap("n", "<leader>gs", vsc_action("workbench.view.scm"), opts)
+keymap("n", "<leader>gd", vsc_action("git.openChange"), opts)
+
+-- ---- Debug ----
+keymap(nv, "<leader>db", vsc_action("editor.debug.action.toggleBreakpoint"), opts)
+
+-- ---- UI / Misc ----
+keymap(nv, "<leader>cp", vsc_action("workbench.action.showCommands"), opts)
+keymap(nv, "<leader>cn", vsc_action("notifications.clearAll"), opts)
+
+-- ====================
+-- Harpoon
+-- ====================
+
 keymap(nv, "<leader>ha", vsc_action("vscode-harpoon.addEditor"))
 keymap(nv, "<leader>ho", vsc_action("vscode-harpoon.editorQuickPick"))
 keymap(nv, "<leader>he", vsc_action("vscode-harpoon.editEditors"))
@@ -64,7 +136,10 @@ keymap(nv, "<leader>h7", vsc_action("vscode-harpoon.gotoEditor7"))
 keymap(nv, "<leader>h8", vsc_action("vscode-harpoon.gotoEditor8"))
 keymap(nv, "<leader>h9", vsc_action("vscode-harpoon.gotoEditor9"))
 
--- project manager keymaps
+-- ====================
+-- Project Manager
+-- ====================
+
 keymap(nv, "<leader>pa", vsc_action("projectManager.saveProject"))
 keymap(nv, "<leader>po", vsc_action("projectManager.listProjectsNewWindow"))
 keymap(nv, "<leader>pe", vsc_action("projectManager.editProjects"))
